166f05411075f0a358f59f1632f7e881842fbd2f
staudohar <130002679+staudohar@users.noreply.github.com>
2025-01-16 19:55:31 -0300

    adicionando contadores de caracteres iguais e diferentes

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,21 @@ abreArquivo caminho = do
   conteudo <- readFile caminho
   return (lines conteudo)
 
+-- Conta caracteres diferentes
+cont_dif :: String -> String -> Int
+cont_dif [] [] = 0
+cont_dif [] m = length m
+cont_dif o [] = length o
+cont_dif (ho:to) (hm:tm) | ho /= hm  = 1 + cont_dif to tm | otherwise = cont_dif to tm
+
+
+-- Conta caracteres iguais
+cont_igual :: String -> String -> Int
+cont_igual [] [] = 0
+cont_igual [] m = 0
+cont_igual o [] = 0
+cont_igual (ho:to) (hm:tm) | ho == hm  = 1 + cont_igual to tm | otherwise = cont_igual to tm
+
 -- B: Onde ocorre a execução principal
 main :: IO ()
 main = do
@@ -10,6 +25,11 @@ main = do
     conteudoOriginal <- abreArquivo "arquivos/original.txt"
     conteudoModificado <- abreArquivo "arquivos/modificado.txt"
 
-    print conteudoOriginal
-    print "\n"
-    print conteudoModificado
+    let result_dif = cont_dif "abobrinha" "abobora" --resultado do contador de caracteres diferentes ok
+    print result_dif
+
+    let result_igual = cont_igual "abobrinha" "abobora" --resultado do contador de caracteres iguais ok
+    print result_igual 
+
+
+
